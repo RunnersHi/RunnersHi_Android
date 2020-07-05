e77dd2f9ac154dc731d5c2ddd40a40f003497476
SuHyeon <kshvv975@gmail.com>
2020-07-05 17:19:26 +0900

    btn_run_lock 이미지 넣음

--- app/src/main/java/com/team/runnershi/RunActivity.kt
+++ app/src/main/java/com/team/runnershi/RunActivity.kt
@@ -2,10 +2,18 @@ package com.team.runnershi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_run.*
 
 class RunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_run)
+
+        initUi()
+    }
+
+    private fun initUi(){
+        Glide.with(this).load(R.drawable.icon_unlock).into(btn_run_lock)
     }
 }
