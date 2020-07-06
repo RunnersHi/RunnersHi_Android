ae25c95ac4ea40f44254b139bfef12946ad2261b
Segon <qkrtprhs@gmail.com>
2020-07-07 01:43:59 +0900

    BottomSheet 띄우기, FINISH UI 제외하고 완료

---
+++ app/src/main/java/com/team/runnershi/HomePagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.team.runnershi
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class HomePagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0 -> HomeFragment()
+            1 -> Fragment2()
+            2 -> Fragment3()
+            else -> Fragment4()
+        }
+    }
+
+    override fun getCount() = 4
+}
