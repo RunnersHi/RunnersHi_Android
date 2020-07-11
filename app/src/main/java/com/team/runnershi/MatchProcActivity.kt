580f9bdf49babf2ebaeff41f19032b746707d5da
SuHyeon <kshvv975@gmail.com>
2020-07-12 00:15:21 +0900

    MatchProcActivity.kt를 SocketService.Receiver 인터페이스를 구현함 MatchProcActivity.kt  SocketService.kt 와 연결하기 위한 socketResultReceiver 만들고 main handler 와 연결함 SocketService.kt로 joinRoom 통신요청하는 sendJoinRoom 함수를 만듬 SocketService.kt로 부터 응답을 받기 위한 onRecieveResult를 만듬 이걸로, LeftTime, OppoenetInfo받아서 뷰 업데이트 함

--- app/src/main/java/com/team/runnershi/MatchProcActivity.kt
+++ app/src/main/java/com/team/runnershi/MatchProcActivity.kt
@@ -4,60 +4,32 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.team.runnershi.SocketService.Companion.LEFT_TIME
+import com.team.runnershi.SocketService.Companion.OPPONENT_INFO
 import kotlinx.android.synthetic.main.activity_match_proc.*
 
-class MatchProcActivity : AppCompatActivity() {
+class MatchProcActivity : AppCompatActivity(), SocketServiceReceiver.Receiver {
     private var runtime = -1
     private var rungender = -1
-    private var leftTime = -1
     private val TAG = MatchProcActivity::class.simpleName
     private lateinit var mService: SocketService
-    private val handler = Handler(Looper.getMainLooper()
-    ) { message ->
-        if(message.what == 1111){
-            progress_match_proc.progress = message.arg1
-        }
-        true
-    }
+    private lateinit var socketResultReceiver: SocketServiceReceiver
 
-
-    private val connection = object : ServiceConnection {
-        override fun onServiceDisconnected(p0: ComponentName?) {
-            Log.d(TAG, "onService Disconnected")
-        }
-
-        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
-            Log.d(TAG, "onService Connected")
-//            val binder = p1 as SocketService.LocalBinder
-//            mService = binder.getService()
-
-
-            val lefttime = runtime * 60
-            val work = Intent().apply {
-                putExtra("serviceFlag", "joinRoom")
-                putExtra("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3Q4IiwicGFzc3dvcmQiOiJ0ZXN0IiwidG9rZW4iOiJ0b2tlbiIsImlhdCI6MTU5NDE4MTE4NiwiZXhwIjoxNTk0MjE3MTg2fQ.72wmGGKZtK4UYhz16M2GHd8pfHF8hz-BlcFOdjo_634")
-                putExtra("time", lefttime)
-                putExtra("wantGender",rungender)
-                putExtra("leftTime",  lefttime)
-            }
-
-
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_proc)
 
         runtime = intent.getIntExtra("runtime", -1)
-        rungender= intent.getIntExtra("rungender", -1)
-
+        rungender = intent.getIntExtra("rungender", -1)
+        socketResultReceiver = SocketServiceReceiver(Handler(Looper.getMainLooper()))
+        socketResultReceiver.reciever = this
     }
 
     override fun onStart() {
@@ -66,27 +38,55 @@ class MatchProcActivity : AppCompatActivity() {
 //            bindService(intent, connection, Context.BIND_AUTO_CREATE)
 //        }
 
-        val lefttime = runtime * 60
+        sendJoinRoom(runtime, rungender)
+    }
+
+    private fun sendJoinRoom(runtime: Int, rungender: Int) {
         val work = Intent()
         work.putExtra("serviceFlag", "joinRoom")
         work.putExtra(
             "token",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3R0ZXN0MTIiLCJwYXNzd29yZCI6InRlc3R0ZXN0MTIiLCJ0b2tlbiI6InRva2VuIiwiaWF0IjoxNTk0NDUzODkxLCJleHAiOjE1OTQ0ODk4OTF9.8BTlABtaYzW14zZZy4klppvB1BSfcW57cZl1WNyX66I"
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3R0ZXN0NyIsInBhc3N3b3JkIjoidGVzdHRlc3Q3IiwidG9rZW4iOiJ0b2tlbiIsImlhdCI6MTU5NDQ3NzI0NiwiZXhwIjoxNTk0NTEzMjQ2fQ.f7XJ6UP9W-hNVPkZFv4czXfKvcYUMwg_mylRBAxl0Ms"
         )
-        work.putExtra("time", lefttime)
+        work.putExtra("time", runtime)
         work.putExtra("wantGender", rungender)
-        work.putExtra("leftTime", lefttime)
-
-//        startService(intent)
+        work.putExtra("leftTime", 300)
+        work.putExtra("receiver", socketResultReceiver)
         SocketService.enqueueWork(this, work)
     }
 
-    val matchHandler = Handler(Looper.getMainLooper(), Handler.Callback {
-        val leftTime = it.arg1
-        return@Callback true
-    })
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onReceiveResult(resultCode: Int, resultData: Bundle) {
+        when (resultCode) {
+            LEFT_TIME -> {
+                val leftTimeFromServer = resultData.getInt("leftTime")
+                progress_match_proc.progress = 300 - leftTimeFromServer
+            }
+            OPPONENT_INFO -> {
+                val name = resultData.getString("name")
+                val level = resultData.getInt("level")
+                val gender = resultData.getInt("gender")
+                val win = resultData.getInt("win")
+                val lose = resultData.getInt("lose")
+                val image = resultData.getInt("image")
+
+                val intent = Intent(this, MatchSucActivity::class.java).also {
+                    it.putExtra("name", name)
+                    it.putExtra("gender", gender)
+                    it.putExtra("win", win)
+                    it.putExtra("lose", lose)
+                    it.putExtra("image", image)
+                    startActivity(it)
+                }
 
 
 
+            }
+            else -> return
+        }
+    }
 
 }
