d98d2e4a6be914c35b8e4ece2c29264d145ede3d
SuHyeon <kshvv975@gmail.com>
2020-07-12 00:23:02 +0900

    seekbar 관련 코드 다 지움

--- app/src/main/java/com/team/runnershi/RunActivity.kt
+++ app/src/main/java/com/team/runnershi/RunActivity.kt
@@ -1,7 +1,13 @@
 package com.team.runnershi
 
+import android.content.Context
+import android.content.res.TypedArray
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.TypedValue
 import android.view.View
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_run.*
@@ -16,51 +22,31 @@ class RunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_run)
-
-
         initUi()
-
     }
 
     private fun initUi() {
         Glide.with(this).load(R.drawable.icon_unlock).into(btn_run_lock)
-        ly_run_profile.x = 100f
-        ly_run_profile.y = 20f
-
         runSelModel = RunSetViewModel()
 
+
+
         seekbar_run_record.setOnTouchListener { _, _ -> true }
-        seekbar_run_record.progress = 25
-        val width: Int = (seekbar_run_record.getWidth()
-                - seekbar_run_record.getPaddingLeft()
-                - seekbar_run_record.getPaddingRight())
-        val thumbPos: Int = (seekbar_run_record.getPaddingLeft()
-                + width
-                * seekbar_run_record.getProgress()
-                / seekbar_run_record.getMax())
-        ly_run_profile.x = thumbPos.toFloat()
-
         seekbar_run_record.max = runSelModel.runTime.value ?: 30
-//        Thread(Runnable {
-//            for (i in 1 until seekbar_run_record.max) {
-//                Thread.sleep(1000)
-//                seekbar_run_record.progress += 1
-//                val width: Int = (seekbar_run_record.getWidth()
-//                        - seekbar_run_record.getPaddingLeft()
-//                        - seekbar_run_record.getPaddingRight())
-//                val thumbPos: Int = (seekbar_run_record.getPaddingLeft()
-//                        + width
-//                        * seekbar_run_record.getProgress()
-//                        / seekbar_run_record.getMax())
-//                ly_run_profile.x = thumbPos.toFloat()
-//
-//            }
-//        }).start()
 
 
-//                val seekBarX = seekbar_run_record.thumb.bounds.exactCenterX()
-//                ly_run_profile.x = seekBarX
-//        val seekBarX = seekbar_run_record.thumb.bounds.exactCenterX() + seekbar_run_record.thumb.bounds.width()
+        Thread(Runnable {
+            for (i in 1 until seekbar_run_record.max) {
+                Thread.sleep(1000)
+                seekbar_run_record.progress += 1
+
+            }
+
+        }).start()
+
+
+
+
 
     }
 
