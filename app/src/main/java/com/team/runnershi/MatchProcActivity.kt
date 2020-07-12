7d955b92ea61e1a23adbbbad10a8a8c0b9efdcef
SuHyeon <kshvv975@gmail.com>
2020-07-13 05:02:58 +0900

    러닝 중단시 사용하기 위한 roomName 변수 만듬 SocketServiceReceiver 가 사용하는 핸들러를 mainLooper 에서 myLooper 로 바꿈 테스트위한 6,7번 토큰 추가함 onStop() 메서드 삭제함 onReceiveResult에서 roomname 저장하는 로직인 RESULT_ROOM_NAME 추가함

--- app/src/main/java/com/team/runnershi/MatchProcActivity.kt
+++ app/src/main/java/com/team/runnershi/MatchProcActivity.kt
@@ -1,26 +1,21 @@
 package com.team.runnershi
 
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.Handler
-import android.os.IBinder
 import android.os.Looper
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.team.runnershi.SocketService.Companion.LEFT_TIME
-import com.team.runnershi.SocketService.Companion.OPPONENT_INFO
+import com.team.runnershi.SocketService.Companion.RESULT_LEFT_TIME
+import com.team.runnershi.SocketService.Companion.RESULT_OPPONENT_INFO
+import com.team.runnershi.SocketService.Companion.RESULT_ROOM_NAME
+import com.team.runnershi.extension.logDebug
 import kotlinx.android.synthetic.main.activity_match_proc.*
 
 class MatchProcActivity : AppCompatActivity(), SocketServiceReceiver.Receiver {
     private var runtime = -1
     private var rungender = -1
-    private val TAG = MatchProcActivity::class.simpleName
-    private lateinit var mService: SocketService
     private lateinit var socketResultReceiver: SocketServiceReceiver
-
+    private var roomName = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +23,7 @@ class MatchProcActivity : AppCompatActivity(), SocketServiceReceiver.Receiver {
 
         runtime = intent.getIntExtra("runtime", -1)
         rungender = intent.getIntExtra("rungender", -1)
-        socketResultReceiver = SocketServiceReceiver(Handler(Looper.getMainLooper()))
+        socketResultReceiver = SocketServiceReceiver(Handler(Looper.myLooper()!!))
         socketResultReceiver.reciever = this
     }
 
@@ -42,7 +37,8 @@ class MatchProcActivity : AppCompatActivity(), SocketServiceReceiver.Receiver {
         work.putExtra("serviceFlag", "joinRoom")
         work.putExtra(
             "token",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3R0ZXN0NyIsInBhc3N3b3JkIjoidGVzdHRlc3Q3IiwidG9rZW4iOiJ0b2tlbiIsImlhdCI6MTU5NDQ3NzI0NiwiZXhwIjoxNTk0NTEzMjQ2fQ.f7XJ6UP9W-hNVPkZFv4czXfKvcYUMwg_mylRBAxl0Ms"
+//            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3R0ZXN0NyIsInBhc3N3b3JkIjoidGVzdHRlc3Q3IiwidG9rZW4iOiJ0b2tlbiIsImlhdCI6MTU5NDU3Mjc4NywiZXhwIjoxNTk0NjA4Nzg3fQ.RoDk0q4f_eHDdwYm2laYNkr6SlLzJTAnrs-jgqMvwrE" // 7
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3R0ZXN0NiIsInBhc3N3b3JkIjoidGVzdHRlc3Q2IiwidG9rZW4iOiJ0b2tlbiIsImlhdCI6MTU5NDU3Mjc3MSwiZXhwIjoxNTk0NjA4NzcxfQ.FeHNyuOozUdtEm4HUQXsQk5JUTyDNHArfWSQ8uR34gM" // 6
         )
         work.putExtra("time", runtime)
         work.putExtra("wantGender", rungender)
@@ -51,17 +47,14 @@ class MatchProcActivity : AppCompatActivity(), SocketServiceReceiver.Receiver {
         SocketService.enqueueWork(this, work)
     }
 
-    override fun onStop() {
-        super.onStop()
-    }
-
     override fun onReceiveResult(resultCode: Int, resultData: Bundle) {
         when (resultCode) {
-            LEFT_TIME -> {
+            RESULT_LEFT_TIME -> {
                 val leftTimeFromServer = resultData.getInt("leftTime")
                 progress_match_proc.progress = 300 - leftTimeFromServer
             }
-            OPPONENT_INFO -> {
+
+            RESULT_OPPONENT_INFO -> {
                 val name = resultData.getString("name")
                 val level = resultData.getInt("level")
                 val gender = resultData.getInt("gender")
@@ -69,17 +62,20 @@ class MatchProcActivity : AppCompatActivity(), SocketServiceReceiver.Receiver {
                 val lose = resultData.getInt("lose")
                 val image = resultData.getInt("image")
 
-                val intent = Intent(this, MatchSucActivity::class.java).also {
-                    it.putExtra("name", name)
-                    it.putExtra("gender", gender)
-                    it.putExtra("win", win)
-                    it.putExtra("lose", lose)
-                    it.putExtra("image", image)
-                    startActivity(it)
-                }
 
-
-
+                val intent = Intent(this, MatchSucActivity::class.java)
+                intent.putExtra("name", name)
+                intent.putExtra("level", level)
+                intent.putExtra("gender", gender)
+                intent.putExtra("win", win)
+                intent.putExtra("lose", lose)
+                intent.putExtra("image", image)
+                intent.putExtra("runtime", runtime)
+                startActivity(intent)
+                finish()
+            }
+            RESULT_ROOM_NAME ->{
+                roomName = resultData.getString("roomName")!!
             }
             else -> return
         }
