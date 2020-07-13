74404ef73c31afb208653b66cac59dac5ede073b
chaeppy <chaeppy108@gmail.com>
2020-07-14 01:20:43 +0900

    랭킹화면-명예의 전당 서버통신을 위한 뷰홀더 RvRankHonorViewHolder.kt를 만듬
    Signed-off-by: chaeppy <chaeppy108@gmail.com>

---
+++ app/src/main/java/com/team/runnershi/recycle/RvRankHonorViewHolder.kt
@@ -0,0 +1,40 @@
+package com.team.runnershi.recycle
+
+import android.graphics.Color
+import android.util.DisplayMetrics
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.team.runnershi.R
+import com.team.runnershi.data.RunnerData
+import com.team.runnershi.data.WinnerData
+import org.w3c.dom.Text
+
+class RvRankHonorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val rank = itemView.findViewById<TextView>(R.id.tv_rank_rank)
+    val profile = itemView.findViewById<ImageView>(R.id.imgv_rank_profile)
+    val nickname = itemView.findViewById<TextView>(R.id.tv_rank_nick_name)
+    val dist = itemView.findViewById<TextView>(R.id.tv_rank_data)
+    val imgArray: Array<Int> = arrayOf(
+        R.drawable.icon_redman_shorthair,
+        R.drawable.icon_blueman_shorthair,
+        R.drawable.icon_redman_basichair,
+        R.drawable.icon_blueman_permhair,
+        R.drawable.icon_redwomen_ponytail,
+        R.drawable.icon_bluewomen_ponytail,
+        R.drawable.icon_redman_shorthair,
+        R.drawable.icon_redwomen_shortmhair,
+        R.drawable.icon_redwomen_bunhair
+    )
+
+    fun bind(winnerData : WinnerData, ranking: Int){
+        rank.text =  ranking.toString()
+        profile.setImageResource(imgArray[winnerData.image - 1])
+        nickname.text = winnerData.nickname
+        //m-> km변환해서 입력
+        dist.text = winnerData.win.toString() + "승 " + winnerData.lose.toString() +"패"
+    }
+
+}
