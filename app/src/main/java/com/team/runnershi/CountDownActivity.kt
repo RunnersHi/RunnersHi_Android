d91b769568ebacdff098a3d516aa892f76fef270
SuHyeon <kshvv975@gmail.com>
2020-07-13 13:22:22 +0900

    CountDownActivity.kt MatchSucActivity.kt 에서 roomName고 매칭 상대방에 대한 정보 받아서 다음화면(StartRunActivity.kt)로 넘어감

--- app/src/main/java/com/team/runnershi/CountDownActivity.kt
+++ app/src/main/java/com/team/runnershi/CountDownActivity.kt
@@ -13,30 +13,54 @@ class CountDownActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_count_down)
 
+        initUi()
+    }
 
+    private fun initUi() {
         ani_coundown_number.playAnimation()
-
-        var intent = Intent(this, StartRunActivity::class.java)
-
-        ani_coundown_number.addAnimatorListener(object : Animator.AnimatorListener{
-            override fun onAnimationRepeat(p0: Animator?) {
-
-            }
-
+        ani_coundown_number.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {}
             override fun onAnimationEnd(p0: Animator?) {
-
+                val intent = Intent(this@CountDownActivity, StartRunActivity::class.java)
+                with(intent) {
+                    putExtra(
+                        "roomName",
+                        intent.getStringExtra("roomName")
+                    )
+                    putExtra(
+                        "name",
+                        intent.getStringExtra("name")
+                    )
+                    putExtra(
+                        "level",
+                        intent.getIntExtra("level", -1)
+                    )
+                    putExtra(
+                        "image",
+                        intent.getIntExtra("image", -1)
+                    )
+                    putExtra(
+                        "win",
+                        intent.getIntExtra("win", -1)
+                    )
+                    putExtra(
+                        "lose",
+                        intent.getIntExtra("lose", -1)
+                    )
+                    putExtra(
+                        "runtime",
+                        intent.getIntExtra("runtime", -1)
+                    )
+                }
                 startActivity(intent)
-
             }
 
-            override fun onAnimationCancel(p0: Animator?) {
-
-            }
-
-            override fun onAnimationStart(p0: Animator?) {
-
-            }
-
+            override fun onAnimationCancel(p0: Animator?) {}
+            override fun onAnimationStart(p0: Animator?) {}
         })
+    }
+
+    private fun GoStartRunActivity() {
+
     }
 }
