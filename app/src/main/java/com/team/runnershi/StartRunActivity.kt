e459cebc51989c1f8dfec6c50a456a9237bc589b
SuHyeon <kshvv975@gmail.com>
2020-07-13 13:25:19 +0900

    StartRunActivity.kt 에서 정보 받아서 RunActivity.kt로 넘어감

--- app/src/main/java/com/team/runnershi/StartRunActivity.kt
+++ app/src/main/java/com/team/runnershi/StartRunActivity.kt
@@ -1,5 +1,6 @@
 package com.team.runnershi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,41 @@ class StartRunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_run)
+
+        goRunActivity()
+    }
+    private fun goRunActivity(){
+        val intent = Intent(this@StartRunActivity, RunActivity::class.java)
+        with(intent) {
+            putExtra(
+                "roomName",
+                intent.getStringExtra("roomName")
+            )
+            putExtra(
+                "name",
+                intent.getStringExtra("name")
+            )
+            putExtra(
+                "level",
+                intent.getIntExtra("level", -1)
+            )
+            putExtra(
+                "image",
+                intent.getIntExtra("image", -1)
+            )
+            putExtra(
+                "win",
+                intent.getIntExtra("win", -1)
+            )
+            putExtra(
+                "lose",
+                intent.getIntExtra("lose", -1)
+            )
+            putExtra(
+                "runtime",
+                intent.getIntExtra("runtime", -1)
+            )
+        }
+        startActivity(intent)
     }
 }
