5b231c9ce979d8905fd303158ae413c8772c9db8
Segon <qkrtprhs@gmail.com>
2020-07-13 16:00:18 +0900

    activity_match_dummy.xml, activity_wait_me.xml 완료

---
+++ app/src/main/java/com/team/runnershi/WaitMeActivity.kt
@@ -0,0 +1,11 @@
+package com.team.runnershi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaitMeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wait_me)
+    }
+}
