6c1ab028c8df1e86d427265f56244f9c6f110735
chaeppy <chaeppy108@gmail.com>
2020-07-14 01:21:35 +0900

    RankItemClass.kt 데이터구조 변경으로 삭제됨
    Signed-off-by: chaeppy <chaeppy108@gmail.com>

--- app/src/main/java/com/team/runnershi/recycle/RankItemClass.kt
+++
@@ -1,9 +0,0 @@
-package com.team.runnershi.recycle
-
-data class RankItemClass(
-    val id : Int, //상세보기에 필요한 아이디값
-    val rank: Int,
-    val profile :Int,
-    val nickname : String,
-    val data : String
-)
