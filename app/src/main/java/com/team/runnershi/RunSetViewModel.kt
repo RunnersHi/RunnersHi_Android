60639058c4df8d4f5d83a7d2855e2283c4a74a34
SuHyeon <kshvv975@gmail.com>
2020-07-14 13:25:10 +0900

    RunSetViewModel.kt 초기남은 시간을 저장해두고, 1초마다 남은 시간을 줄이고, progress 바를 늘리는 ViewModel 을 만듬

--- app/src/main/java/com/team/runnershi/RunSetViewModel.kt
+++ app/src/main/java/com/team/runnershi/RunSetViewModel.kt
@@ -1,21 +1,57 @@
 package com.team.runnershi
 
+import android.location.Location
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.team.runnershi.extension.logDebug
+import java.util.*
 
 class RunSetViewModel : ViewModel() {
-    var runTime: MutableLiveData<Int> = MutableLiveData()
+    val ldRunLeftTime = MutableLiveData<String>("00:00:00")
+    val ldRunProgress = MutableLiveData<Int>()
+    private var timer = Timer()
+    private var prevLeftTime = -1
+    private var prevProgress = -1
+    private var leftTime = 0
 
-    fun changeRunTimeToHour(runTime: MutableLiveData<Int>): String
-        = when(runTime.value){
-            30 -> "00:30"
-            45 -> "00:45"
-            60 -> "01:00"
-            90 -> "01:30"
-            else -> "--:--"
-        }
+    val ldCurrentLocation = MutableLiveData<Location>()
 
 
+
+    fun showRunLeftTime(prevLeftTime: Int) {
+        "Show Run Left Time (prevLeftTime: $prevLeftTime)".logDebug(
+            this@RunSetViewModel
+        )
+        this.prevLeftTime = prevLeftTime
+        "initial (prevLeftTime $prevLeftTime) (prevProgress: $prevProgress)".logDebug(this@RunSetViewModel)
+
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                leftTime += 1
+                val newLeftTime = this@RunSetViewModel.prevLeftTime - leftTime
+                val hour = newLeftTime / (60 * 60)
+                val minutes = newLeftTime % (60 * 60) / 60
+                val seconds = newLeftTime % (60 * 60) % 60
+
+                if (hour == 0) {
+                    ldRunLeftTime.postValue("$minutes:$seconds")
+                    "timer runLeftTime.postValue : ($minutes:$seconds)".logDebug(this@RunSetViewModel)
+                } else {
+                    ldRunLeftTime.postValue("$hour:$minutes:$seconds")
+                    "timer runLeftTime.postValue : ($hour:$minutes:$seconds)".logDebug(this@RunSetViewModel)
+                }
+
+
+                "timer runProgress.postValue: $leftTime"
+                ldRunProgress.postValue(leftTime)
+
+            }
+        }, ONE_SECOND, ONE_SECOND)
+    }
+
+    companion object {
+        const val ONE_SECOND = 1000L
+    }
 
 }
 
