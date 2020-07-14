aea4fa7ddbff7238ab53b155ee76698a28b98bd4
chaeppy <chaeppy108@gmail.com>
2020-07-14 21:47:43 +0900

    result 폴더링으로 인해 삭제됨
    Signed-off-by: chaeppy <chaeppy108@gmail.com>

--- app/src/main/java/com/team/runnershi/result/ResultActivity.kt
+++ app/src/main/java/com/team/runnershi/result/ResultActivity.kt
@@ -1,7 +1,8 @@
-package com.team.runnershi
+package com.team.runnershi.result
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.team.runnershi.R
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
