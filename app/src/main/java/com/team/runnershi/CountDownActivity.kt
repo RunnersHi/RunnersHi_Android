46382ccd220a54e50ccd60ccf1982beab07b0b88
chaeppy <chaeppy108@gmail.com>
2020-07-16 22:09:43 +0900

    CountDownActivity.kt수정함 이유: 진입지점이 매칭성공시인지, 매칭실패시인지 roomName으로 구분하여 인텐트 다르게 전달
    Signed-off-by: chaeppy <chaeppy108@gmail.com>

--- app/src/main/java/com/team/runnershi/CountDownActivity.kt
+++ app/src/main/java/com/team/runnershi/CountDownActivity.kt
@@ -4,6 +4,7 @@ import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.team.runnershi.data.RecordRunWithmeData
 import com.team.runnershi.extension.logDebug
 import kotlinx.android.synthetic.main.activity_count_down.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
@@ -23,33 +24,50 @@ class CountDownActivity : AppCompatActivity() {
         ani_coundown_number.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationRepeat(p0: Animator?) {}
             override fun onAnimationEnd(p0: Animator?) {
-                val roomName = intent.getStringExtra("roomName")
-                val name = intent.getStringExtra("name")
-                val level = intent.getIntExtra("level", -1)
-                val image = intent.getIntExtra("image", -1)
-                val win = intent.getIntExtra("win", -1)
-                val lose = intent.getIntExtra("lose", -1)
-                val runtime = intent.getIntExtra("runtime", -1)
-
-                "(roomName ${roomName}) (name: $name) (level :$level) (image: $image) (win: $win) (lose: $lose) (runtime: $runtime)".logDebug(this@CountDownActivity)
-
-                val intent = Intent(this@CountDownActivity, StartRunActivity::class.java)
-                with(intent) {
-                    putExtra("roomName", roomName)
-                    putExtra("name", name)
-                    putExtra("level", level)
-                    putExtra("image", image)
-                    putExtra("win", win)
-                    putExtra("lose", lose)
-                    putExtra("runtime", runtime)
-                }
-                startActivity(intent)
-                finish()
+                if (intent.hasExtra("roomName"))
+                    runWithOpponent()
+                else
+                    runWithMe()
             }
 
             override fun onAnimationCancel(p0: Animator?) {}
             override fun onAnimationStart(p0: Animator?) {}
         })
+    }
+
+    fun runWithOpponent() {
+        val roomName = intent.getStringExtra("roomName")
+        val name = intent.getStringExtra("name")
+        val level = intent.getIntExtra("level", -1)
+        val image = intent.getIntExtra("image", -1)
+        val win = intent.getIntExtra("win", -1)
+        val lose = intent.getIntExtra("lose", -1)
+        val runtime = intent.getIntExtra("runtime", -1)
+
+        "(roomName ${roomName}) (name: $name) (level :$level) (image: $image) (win: $win) (lose: $lose) (runtime: $runtime)".logDebug(
+            this@CountDownActivity
+        )
+
+        val intent = Intent(this@CountDownActivity, StartRunActivity::class.java)
+        with(intent) {
+            putExtra("roomName", roomName)
+            putExtra("name", name)
+            putExtra("level", level)
+            putExtra("image", image)
+            putExtra("win", win)
+            putExtra("lose", lose)
+            putExtra("runtime", runtime)
+        }
+        startActivity(intent)
+        finish()
+    }
+
+    fun runWithMe() {
+        val matchData = intent.getParcelableExtra<RecordRunWithmeData>("matchData")
+        val intent = Intent(this@CountDownActivity, StartRunActivity::class.java)
+        intent.putExtra("matchData", matchData)
+        startActivity(intent)
+        finish()
     }
 
 }
