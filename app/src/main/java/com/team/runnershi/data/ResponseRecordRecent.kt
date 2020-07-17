ea3ab7ad1f39bf265d5359d2f3e1758eac6cea1d
SuHyeon <kshvv975@gmail.com>
2020-07-17 15:02:30 +0900

    ReponseRecordRecent.kt 변수들 변경 b) 서버 변경

--- app/src/main/java/com/team/runnershi/data/ResponseRecordRecent.kt
+++ app/src/main/java/com/team/runnershi/data/ResponseRecordRecent.kt
@@ -1,9 +1,15 @@
 package com.team.runnershi.data
 
+import com.google.gson.annotations.SerializedName
+
 data class ResponseRecordRecent(
+    @SerializedName("status")
     val status : Int,
+    @SerializedName("success")
     val success : Boolean,
+    @SerializedName("message")
     val message : String,
-    val result : AllRecordRecentContent
+    @SerializedName("result")
+    val result : AllRecordRecentContent?
 
 )
