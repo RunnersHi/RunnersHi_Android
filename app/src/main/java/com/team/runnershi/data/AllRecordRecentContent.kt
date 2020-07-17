e323e8c763b9617623688cd84c30827e9b49bcc1
SuHyeon <kshvv975@gmail.com>
2020-07-17 15:02:03 +0900

    AllRecordRecentContent.kt 변수들 변경 b) 서버 변경

--- app/src/main/java/com/team/runnershi/data/AllRecordRecentContent.kt
+++ app/src/main/java/com/team/runnershi/data/AllRecordRecentContent.kt
@@ -1,7 +1,20 @@
 package com.team.runnershi.data
 
+import com.google.gson.annotations.SerializedName
+
 data class AllRecordRecentContent(
-    val distance : Int,
-    val time : String,
-    val pace : Int
+    @SerializedName("distance")
+    val distance: Int,
+    @SerializedName("time")
+    val time: String,
+    @SerializedName("pace_minute")
+    val paceMinute: Int,
+    @SerializedName("pace_second")
+    val paceSecond: Int,
+    @SerializedName("image")
+    val image: Int,
+    @SerializedName("result")
+    val result: Int,
+    @SerializedName("created_time")
+    val createdTime: String
 )
