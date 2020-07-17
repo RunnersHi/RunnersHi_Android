f6538003635de135f707cbbb72a2ca70a231be86
chaeppy <chaeppy108@gmail.com>
2020-07-17 16:28:22 +0900

    나와 경쟁하기 러닝중 화면을 위한 RunMeSetViewModel.kt만듬
    Signed-off-by: chaeppy <chaeppy108@gmail.com>

---
+++ app/src/main/java/com/team/runnershi/RunMeSetViewModel.kt
@@ -0,0 +1,110 @@
+package com.team.runnershi
+import android.location.Location
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.naver.maps.geometry.LatLng
+import com.team.runnershi.extension.logDebug
+import java.util.*
+
+class RunMeSetViewModel : ViewModel() {
+    val ldRunLeftTime = MutableLiveData<String>("00:00:00")
+    val ldRunProgress = MutableLiveData<Int>()
+    val ldTimeOver = MutableLiveData<Boolean>()
+    var timer = Timer()
+    private var runTime = -1
+    private var prevProgress = -1
+    private var leftTime = 0
+    val ldCurrentLocation = MutableLiveData<Location>()
+    val ldPath = MutableLiveData<MutableList<LatLng>>()
+    val path = mutableListOf<LatLng>()
+    var prevLocation: Location? = null
+    var distStandard = 1
+    val ldTotalMeter = MutableLiveData<Int>(0)
+    val ldTotalDistString = MutableLiveData<String>("0.00")
+    val ldPace = MutableLiveData<String>("00\'00\"")
+    val ldIsKmPassed = MutableLiveData<Boolean>(false)
+    fun calTotalDistance(curLocation: Location) {
+        if (prevLocation == null) {
+            prevLocation = curLocation
+        } else {
+            val interval = prevLocation!!.distanceTo(curLocation).toInt()
+            ldTotalMeter.value?.let { curTotalMeter ->
+                ldTotalMeter.postValue(curTotalMeter + interval)
+            }
+            val totalKm = ldTotalMeter.value?.toFloat()?.div(1000)
+            ldTotalDistString.postValue(String.format("%.2f", totalKm))
+        }
+    }
+    fun checkKmPassed(totalMeter: Int) {
+//        ldIsKmPassed.value = (totalMeter / 1000 >= distStandard)
+        ldIsKmPassed.value = (totalMeter/5 >= distStandard)
+    }
+    fun incDistStandard() {
+        distStandard += 1
+    }
+    fun calPace(curLocation: Location) {
+        if (prevLocation == null) {
+            prevLocation = curLocation
+        } else {
+            val timeIntervalSec = (curLocation.time - prevLocation!!.time).toFloat() / 1000
+            val distIntervalMeter = prevLocation!!.distanceTo(curLocation)
+            val paceMinute =
+                if (distIntervalMeter == 0f) {
+                    0f
+                } else {
+                    (timeIntervalSec / 60f) / (distIntervalMeter / 1000f)
+                }
+            val paceSeconds =
+                if (distIntervalMeter == 0f) 0f
+                else {
+                    (paceMinute - paceMinute.toInt().toFloat()) * 60f
+                }
+            if (paceMinute >= 60) {
+                ldPace.postValue("-'--\"")
+                "Pace PostValue: -'--\"".logDebug(this@RunMeSetViewModel)
+            } else {
+                ldPace.postValue("${paceMinute.toInt()}'${paceSeconds.toInt()}\"")
+                "Pace PostValue (paceMinute.toInt(): ${paceMinute.toInt()} paceSeconds: ${paceSeconds.toInt()})".logDebug(
+                    this@RunMeSetViewModel
+                )
+            }
+        }
+    }
+    fun showRunLeftTime(prevLeftTime: Int) {
+        "Show Run Left Time (prevLeftTime: $prevLeftTime)".logDebug(
+            this@RunMeSetViewModel
+        )
+        this.runTime = prevLeftTime
+        "initial (prevLeftTime $prevLeftTime) (prevProgress: $prevProgress)".logDebug(this@RunMeSetViewModel)
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                leftTime += 1
+                val newLeftTime = this@RunMeSetViewModel.runTime - leftTime
+                val hour = newLeftTime / (60 * 60)
+                val minutes = newLeftTime % (60 * 60) / 60
+                val seconds = newLeftTime % (60 * 60) % 60
+                val strHour = if (hour == 0) "" else "$hour:"
+                val strMinutes = if (minutes >= 10) "$minutes:" else "0$minutes:"
+                val strSeconds = "$seconds"
+                ldRunLeftTime.postValue("$strHour$strMinutes$strSeconds")
+                "timer runLeftTime.postValue (hour: $hour) (minutes: $minutes) (seconds: $seconds)".logDebug(
+                    this
+                )
+                ldRunProgress.postValue(leftTime)
+            }
+        }, ONE_SECOND, ONE_SECOND)
+    }
+    fun pathUpdate(currentLocation: Location) {
+        path.add(LatLng(currentLocation))
+        if (path.size >= 2) {
+            ldPath.postValue(path)
+        }
+    }
+
+    fun checkTimerOver(passTime: Int) {
+        ldTimeOver.value = (runTime <= passTime)
+    }
+    companion object {
+        const val ONE_SECOND = 1000L
+    }
+}
