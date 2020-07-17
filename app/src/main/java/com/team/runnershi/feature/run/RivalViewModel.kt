d62b1618a417c3d9a51c92582c5ee0d4d2273a56
SuHyeon <kshvv975@gmail.com>
2020-07-17 21:35:45 +0900

    RunActivity.kt 에서 버튼 클릭시 버튼 색깔 변동 위함

--- app/src/main/java/com/team/runnershi/feature/run/RivalViewModel.kt
+++ app/src/main/java/com/team/runnershi/feature/run/RivalViewModel.kt
@@ -1,2 +1,16 @@
 package com.team.runnershi.feature.run
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RivalViewModel : ViewModel() {
+    val isClickable = MutableLiveData<Boolean>(false)
+
+    fun checkClickable(selectedGender: Int) {
+        isClickable.postValue(
+            (selectedGender == 1
+                    || selectedGender == 2
+                    || selectedGender == 3)
+        )
+    }
+}
